87b7dda0af21d3bb0984565b103f6b8155315366
richifg <richifg@gmail.com>
2019-05-13 18:23:45 -0600

    added TextBox comp

---
+++ src/components/InputTextBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import fonts from '../fonts';
+import colors from '../colors';
+
+const Input = styled.textarea`
+ ${fonts.defaultInput}
+  background: ${colors.background};
+  box-sizing: border-box;
+  width: ${props => props.fill ? " 100%;" : "360px"};
+  height:${props => props.fill ? " 100%;" : "108px"};
+  border-radius: 18px;
+  border: 1px ${colors.passive} solid;
+  padding-left: 15px;
+  padding-top: 10px;
+  resize: none;
+`
+
+export default ({placeholder, value, fill}) => <Input placeholder={placeholder} value={value} fill={fill}></Input>;
+
+
---
+++ stories/InputTextBox.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import InputTextBox from '../src/components/InputTextBox';
+
+
+storiesOf('InputTextBox', module)
+    .add('Fill available space', () =>([
+        <p>Available Space</p>,
+        <div style={{width: "500px", height: "200px", padding:"10px", border:" 1px dashed"}}><InputTextBox fill/></div>
+    ]))
+    .add('default size - With placeholder', () =>(
+        <InputTextBox placeholder="Direccion exacta"/>
+    ))
+    .add('default size - With values', () =>(
+        <InputTextBox>500m Norte del Parque Central</InputTextBox>
+    ))
+    .add('default size - Empty', () =>(
+        <InputTextBox/>
+    ))
